9995e9c3408dbba9c15adf573b88f8b1a6b64df0
Kaan <krall2626@gmail.com>
2025-04-26 21:16:12 +0300

    Otomatik senkron

--- lib/ana_sayfa_icerik.dart
+++ lib/ana_sayfa_icerik.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'weather_service.dart';
 import 'city_selection.dart';
 import 'package:intl/intl.dart';
-import 'package:intl/date_symbol_data_local.dart'; // <<< EKLENDİ
+import 'package:intl/date_symbol_data_local.dart';
 
 class AnaSayfaIcerik extends StatefulWidget {
   const AnaSayfaIcerik({super.key});
@@ -19,7 +19,7 @@ class _AnaSayfaIcerikState extends State<AnaSayfaIcerik> {
   @override
   void initState() {
     super.initState();
-    initializeDateFormatting('tr_TR', null).then((_) {  // <<< EKLENDİ
+    initializeDateFormatting('tr_TR', null).then((_) {
       _fetchWeather();
     });
   }
@@ -37,5 +37,77 @@ class _AnaSayfaIcerikState extends State<AnaSayfaIcerik> {
     });
   }
 
-  ...
+  void _selectCity() async {
+    final selectedCity = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const CitySelectionPage()),
+    );
+
+    if (selectedCity != null) {
+      setState(() {
+        _city = selectedCity;
+      });
+      _fetchWeather();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _isLoading
+        ? const Center(child: CircularProgressIndicator())
+        : _buildWeatherContent();
+  }
+
+  Widget _buildWeatherContent() {
+    if (_forecastWeather == null) {
+      return const Center(child: Text('Veri alınamadı'));
+    }
+
+    List<dynamic> forecastList = _forecastWeather!['list'];
+
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '📍 $_city',
+                style: const TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+              IconButton(
+                icon: const Icon(Icons.location_city),
+                onPressed: _selectCity,
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 7,
+              itemBuilder: (context, index) {
+                final weatherData = forecastList[index * 8];
+                DateTime date = DateTime.now().add(Duration(days: index));
+                String formattedDate = DateFormat('EEEE, dd MMMM', 'tr_TR').format(date);
+
+                return Card(
+                  child: ListTile(
+                    leading: Image.network(
+                      'https://openweathermap.org/img/wn/${weatherData['weather'][0]['icon']}@2x.png',
+                    ),
+                    title: Text(formattedDate),
+                    subtitle: Text(
+                      '${weatherData['weather'][0]['description']} - '
+                          '${weatherData['main']['temp'].toStringAsFixed(0)}°C',
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
